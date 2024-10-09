c925c9dadb1b548e8a7ddbb5d54794908f36cf97
Yodieru <163355601+Yodieru@users.noreply.github.com>
2024-10-09 12:46:20 -0600

    Correccion

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var nombre = prompt ("¿Cual es tu nombre?");
 var edad = prompt ("¿Cual es tu edad?") ;
-var estudiante = true;
+var estudiaste = true;
 
 console.log(nombre);
 console.log(edad);
-console.log(estudiante);
+console.log(estudiaste);
 
 let mensaje = `¡Hola, soy ${nombre} y tengo ${edad} años!`;
 console.log(mensaje);
 
-if (estudiante >= true) {alert ("Gadiel si estudiaste! Pasaras la materia!")} 
-else { alert ("Gadiel no estudiaste! Reprobaras la materia!")}
+if (estudiaste >= true) {alert (nombre + "si estudiaste! Pasaras la materia!")} 
+else { alert (nombre + "no estudiaste! Reprobaras la materia!")} 
